2261c0298d384c5b845775196e9e0897d3240203
sahruldotid <sahruldotid@gmail.com>
2024-02-11 12:10:17 +0700

    first commit

---
+++ .config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,40 @@
+local plugins = {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+        "pyright",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "github/copilot.vim",
+    lazy = false,
+    config = function ()
+      vim.g.copilot_no_tab_map = true;
+      vim.g.copilot_assume_mapped = true;
+      vim.g.copilot_tab_fallback = "";
+    end,
+  },
+  {
+   "typicode/bg.nvim",
+    lazy = false,
+  },
+  {
+    "andweeb/presence.nvim",
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  }
+}
+
+return plugins
