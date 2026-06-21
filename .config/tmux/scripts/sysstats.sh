0d4a4fec802daa32d0310100560578c6cc068997
Syahrul Akbar Rohmani <sahruldotid@gmail.com>
2026-06-21 16:03:57 +0700

    Add Windows OS support for CPU and RAM usage
    Updated sysstats.sh to support Windows OS detection and resource usage retrieval using PowerShell commands.

--- .config/tmux/scripts/sysstats.sh
+++ .config/tmux/scripts/sysstats.sh
@@ -3,31 +3,39 @@
 OS="$(uname)"
 
 if [[ "$OS" == "Linux" ]]; then
-  # CPU usage (%)
-  cpu_idle_1=$(awk '/^cpu / {print $5}' /proc/stat)
-  cpu_total_1=$(awk '/^cpu / {sum=0; for(i=2;i<=NF;i++) sum+=$i; print sum}' /proc/stat)
-  sleep 0.1
-  cpu_idle_2=$(awk '/^cpu / {print $5}' /proc/stat)
-  cpu_total_2=$(awk '/^cpu / {sum=0; for(i=2;i<=NF;i++) sum+=$i; print sum}' /proc/stat)
+  osrelease=$(</proc/sys/kernel/osrelease)
 
-  cpu_delta=$((cpu_total_2 - cpu_total_1))
-  idle_delta=$((cpu_idle_2 - cpu_idle_1))
-
-  if [[ $cpu_delta -eq 0 ]]; then
-    cpu_pct=0
+  if [[ "$osrelease" == *Microsoft* ]] && command -v powershell.exe &>/dev/null; then
+    cpu_pct=$(powershell.exe -NoProfile -NonInteractive -Command "Get-CimInstance Win32_Processor | Select-Object -ExpandProperty LoadPercentage" 2>/dev/null | tail -1 | tr -d '\r')
+    mem_total=$(powershell.exe -NoProfile -NonInteractive -Command "Get-CimInstance Win32_OperatingSystem | Select-Object -ExpandProperty TotalVisibleMemorySize" 2>/dev/null | tail -1 | tr -d '\r')
+    mem_free=$(powershell.exe -NoProfile -NonInteractive -Command "Get-CimInstance Win32_OperatingSystem | Select-Object -ExpandProperty FreePhysicalMemory" 2>/dev/null | tail -1 | tr -d '\r')
+    mem_used=$((mem_total - mem_free))
+    ram_pct=$((mem_used * 100 / mem_total))
   else
-    cpu_pct=$(( (cpu_delta - idle_delta) * 100 / cpu_delta ))
-  fi
+    cpu_idle_1=$(awk '/^cpu / {print $5}' /proc/stat)
+    cpu_total_1=$(awk '/^cpu / {sum=0; for(i=2;i<=NF;i++) sum+=$i; print sum}' /proc/stat)
+    sleep 0.1
+    cpu_idle_2=$(awk '/^cpu / {print $5}' /proc/stat)
+    cpu_total_2=$(awk '/^cpu / {sum=0; for(i=2;i<=NF;i++) sum+=$i; print sum}' /proc/stat)
 
-  # RAM usage (%)
-  meminfo=$(grep -E '^(MemTotal|MemAvailable):' /proc/meminfo | awk '{print $2}')
-  mem_total=$(echo "$meminfo" | sed -n '1p')
-  mem_avail=$(echo "$meminfo" | sed -n '2p')
+    cpu_delta=$((cpu_total_2 - cpu_total_1))
+    idle_delta=$((cpu_idle_2 - cpu_idle_1))
 
-  if [[ $mem_total -eq 0 ]]; then
-    ram_pct=0
-  else
-    ram_pct=$(( (mem_total - mem_avail) * 100 / mem_total ))
+    if [[ $cpu_delta -eq 0 ]]; then
+      cpu_pct=0
+    else
+      cpu_pct=$(( (cpu_delta - idle_delta) * 100 / cpu_delta ))
+    fi
+
+    meminfo=$(grep -E '^(MemTotal|MemAvailable):' /proc/meminfo | awk '{print $2}')
+    mem_total=$(echo "$meminfo" | sed -n '1p')
+    mem_avail=$(echo "$meminfo" | sed -n '2p')
+
+    if [[ $mem_total -eq 0 ]]; then
+      ram_pct=0
+    else
+      ram_pct=$(( (mem_total - mem_avail) * 100 / mem_total ))
+    fi
   fi
 
 elif [[ "$OS" == "Darwin" ]]; then
